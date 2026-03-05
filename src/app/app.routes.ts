4487fd3234a7cfb350518adf79de14ea208487f6
polina <polinaa.gelman@gmail.com>
2026-03-05 23:11:11 +0300

    add menu and start pages + some changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {Start} from './start/start';
+import {Menu} from './menu/menu';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: Start},
+  {path: 'menu', component: Menu},
+];
---
+++ src/app/start/start.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import {RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-start',
+  imports: [RouterLink],
+  templateUrl: './start.html',
+  styleUrl: './start.css',
+})
+export class Start {}
