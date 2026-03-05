76c16fd166dffb9a2c2b5012b31054ac9ecb498b
polina <polinaa.gelman@gmail.com>
2026-03-05 23:55:47 +0300

    add simple menu checkboxes

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import {NgForOf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
+interface MenuItem {
+  name: string;
+  value: number;
+  selected: boolean;
+}
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [
+    NgForOf,
+    FormsModule
+  ],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
-export class Menu {}
+export class Menu {
+  items:  MenuItem[] = [
+    { name: 'Маргарита', value: 500, selected: false },
+    { name: 'Пепперони', value: 600, selected: false },
+    { name: 'Четыре сыра', value: 700, selected: false },
+    { name: 'Гавайская', value: 550, selected: false }
+  ];
+
+  get selectedItems(): number {
+    return this.items.filter(item => item.selected).length;
+  }
+
+  get sumCount(): number {
+    return this.items
+      .filter(item => item.selected)
+      .reduce((sum, item) => sum + item.value, 0);
+  }
+
+}
